d32930c47559e6655a71828d454eb6c98c511694
Isaac <isaac.ruiz0219@gmail.com>
2022-10-28 02:44:05 -0700

    finished the mbvp

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-let addItemInput = document.querySelector('#addItemInput').value;
+let addItemForm = document.querySelector('form');
+let items = document.querySelector('li');
 
-console.log(addItemInput)
+const handleSubmitForm = (e) => {
+    e.preventDefault();
+    let addItemInput = document.querySelector('#addItemInput').value;
+    if (addItemInput != ''){
+        addTodo(addItemInput);
+    }
+    addItemInput.value = '';
+    addItemForm.reset()
+};
+
+addItemForm.addEventListener('submit',handleSubmitForm);
+
+function addTodo(todo) {
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    
+    let bgColor = "#32ae85"
+    li.addEventListener('click',(e) => {
+        li.style.color = bgColor
+        let button = li.childNodes[1]
+        button.style.backgroundColor = bgColor
+        console.log(li.childNodes)
+        setTimeout(()=>li.style.textDecoration = 'line-through',350)
+    })
+
+    li.innerHTML = `
+    <button></button>
+    <span>${todo}</span>
+    `;
+    li.classList.add('todo-list-item');
+    ul.appendChild(li);
+    console.log(items)
+};
+
+console.log(items)
